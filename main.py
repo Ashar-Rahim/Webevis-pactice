dbd464c12722818d14d3e9f277b2848f6947f314
Ashar-Rahim <44979842+Ashar-Rahim@users.noreply.github.com>
2022-02-09 16:38:48 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -87,5 +87,49 @@ def upload_file():
         return resp
 
 
+@app.route('/compress', methods=['POST'])
+def compress():
+    # check if the post request has the file part
+    if 'files[]' not in request.files:
+        resp = jsonify({'message': 'No file part in the request'})
+        resp.status_code = 400
+        return resp
+
+    files = request.files.getlist('files[]')
+    qual = request.form.get('qual')
+
+    errors = {}
+    success = False
+
+    for file in files:
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            success = True
+
+            user_image = 'static\\img\\' + filename
+            picture = Image.open(user_image)
+            print(picture)
+
+            picture.save("static\\img\\Compressed_" + filename, optimize=True, quality=int(qual))
+
+        else:
+            errors[file.filename] = 'File type is not allowed'
+
+    if success and errors:
+        errors['message'] = 'File(s) successfully uploaded'
+        resp = jsonify(errors)
+        resp.status_code = 500
+        return resp
+    if success:
+        resp = jsonify({'message': 'Files successfully uploaded'})
+        resp.status_code = 201
+        return send_file("static\\img\\Compressed_" + filename)
+    else:
+        resp = jsonify(errors)
+        resp.status_code = 500
+        return resp
+
+
 if __name__ == '__main__':
     app.run(debug=True)
