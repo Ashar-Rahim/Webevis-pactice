9d63c9f1476a691ceca127d6b011726054cf3a6f
Ashar-Rahim <44979842+Ashar-Rahim@users.noreply.github.com>
2022-02-10 15:12:13 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ from flask import Flask, request, jsonify, send_file
 import os
 from PIL import Image, ImageFilter
 from werkzeug.utils import secure_filename
+from PIL import ImageEnhance
+import gunicorn
 import sys
- 
+
 app = Flask(__name__)
- 
+
 app.secret_key = "Shary_coder_1925"
- 
+
 UPLOAD_FOLDER = 'static\\img'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
- 
+
 ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
 
 
@@ -53,25 +55,22 @@ def upload_file():
         resp = jsonify({'message': 'No file part in the request'})
         resp.status_code = 400
         return resp
- 
+
     files = request.files.getlist('files[]')
     fil = request.form.get('fil')
-     
+
     errors = {}
     success = False
-     
+
     for file in files:
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             success = True
 
-            user_image = 'static\\img\\' + filename
-            new_image = image_filtering(user_image, fil)
-            new_image.save("static\\img\\newData\\new_img.png")
         else:
             errors[file.filename] = 'File type is not allowed'
- 
+
     if success and errors:
         errors['message'] = 'File(s) successfully uploaded'
         resp = jsonify(errors)
@@ -80,46 +79,7 @@ def upload_file():
     if success:
         resp = jsonify({'message': 'Files successfully uploaded'})
         resp.status_code = 201
-        return send_file("static\\img\\newData\\new_img.png")
-    else:
-        resp = jsonify(errors)
-        resp.status_code = 500
         return resp
-
-
-@app.route('/compress', methods=['POST'])
-def compress():
-    # check if the post request has the file part
-    if 'files[]' not in request.files:
-        resp = jsonify({'message': 'No file part in the request'})
-        resp.status_code = 400
-        return resp
-
-    files = request.files.getlist('files[]')
-    qual = request.form.get('qual')
-
-    errors = {}
-    success = False
-
-    for file in files:
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            success = True
-
-            user_image = 'static\\img\\' + filename
-            picture = Image.open(user_image)
-            print(picture)
-
-            picture.save("static\\img\\Compressed_" + filename, optimize=True, quality=int(qual))
-
-        else:
-            errors[file.filename] = 'File type is not allowed'
-
-    if success:
-        resp = jsonify({'message': 'Files successfully uploaded'})
-        resp.status_code = 201
-        return send_file("static\\img\\Compressed_" + filename)
     else:
         resp = jsonify(errors)
         resp.status_code = 500
@@ -128,3 +88,4 @@ def compress():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
